03688e0351e7b3c08b3a7cbacc6be348ec72916d
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-08-25 14:06:06 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-
+const getRandomHexColor = () => {
+    document.body.style.background = 
+     `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      }`;
+  // print randomly selected color in place "-"
+      printColor.innerHTML = document.body.style.background;  
+  }
+  
+  const buttonStart = document.querySelector("[data-start]");
+  const buttonStop = document.querySelector("[data-stop]");
+  
+  buttonStart.addEventListener("click", () => {
+      timerId = setInterval(() => {
+        getRandomHexColor();
+      }, 1000);
+      // to avoid clicking endlessly
+      buttonStart.disabled = true;
+      console.log("the start button has been clicked and the body changes color every 1s");
+    });
+    
+  buttonStop.addEventListener("click", () => {
+      clearInterval(timerId);
+      // make the button active 
+      buttonStart.disabled = false;
+      console.log("the stop button was clicked to stop the body color changing");
+    });
+  
